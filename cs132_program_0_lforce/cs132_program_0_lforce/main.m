1df494ec4002680dedd85d1f06ce30267f1ed0a5
Guest User <Guest@Mac110-Rakdos.local>
2013-01-21 12:35:33 -0500

    "turning in program 0 part A"

---
+++ cs132_program_0_lforce/cs132_program_0_lforce/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cs132_program_0_lforce
+//
+//  Created by Guest User on 1/21/13.
+//  Copyright (c) 2013 Larry Force. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
