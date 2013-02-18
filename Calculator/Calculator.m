cbdda95052e4603384e6c6b2a664196d475f5d02
Larry Force <lforce@Mac110-Rakdos.local>
2013-02-18 17:24:19 -0500

    Redid batch 3, it is seeming quicker this time, and far more successful.
    Calculator starts up, also on a side note, Excel seems very easy

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -2,6 +2,12 @@
 
 @implementation Calculator
 
+@synthesize numberOnScreen;
+
+@synthesize numberAccumulated;
+
+@synthesize operatingPending;
+
 - (id)init
 {
     self = [super init];
@@ -14,6 +20,14 @@
 
 -(NSString*) description
 {
-    return [NSString stringWithFormat:@"Calculator with %d on screen.", NAN];
+    return [NSString stringWithFormat:@"Calculator with %d on screen.", [self numberOnScreen ]];
 }
+
+
+-(void) pressKey:(char)someUnnamedArgument
+{
+NSLog(@"STUB response to '%@' message received by object at %p (%@)", NSStringFromSelector(_cmd), self, self);
+return;
+}
+
 @end
