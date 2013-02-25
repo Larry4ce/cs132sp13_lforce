813ccefa6ae7637b450d23657348d417c66a6fd2
Larry Force <lforce@Mac110-Rakdos.local>
2013-02-24 22:07:36 -0500

    P2a setup

---
+++ FractionClass/ImmutableFraction.h
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.h
+//  
+//
+//  Created by Larry Force on 2/24/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ImmutableFraction : NSObject
+
+@end
---
+++ FractionClass/ImmutableFraction.m
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.m
+//  
+//
+//  Created by Larry Force on 2/24/13.
+//
+//
+
+#import "ImmutableFraction.h"
+
+@implementation ImmutableFraction
+
+@end
