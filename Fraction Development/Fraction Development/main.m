0659fa2359e9aca3275249c7e8dac5b4eb593e24
Larry Force <lforce@Mac110-Rakdos.local>
2013-04-03 12:07:22 -0400

    first check complete

--- Fraction Development/Fraction Development/main.m
+++ Fraction Development/Fraction Development/main.m
@@ -7,8 +7,36 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "WCSFraction.h"
+#import "WCSMutableFraction.h"
 
 int main(int argc, char *argv[])
 {
+    
+    printf("Checking if 1/3 - 1/2 works... \n") ;
+    
+    WCSFraction* first;
+    WCSFraction* second;
+    WCSFraction* answer;
+    WCSFraction* goal;
+    
+    first = [first initWithNumerator:1 andDenominator:3] ;
+    second = [second initWithNumerator:1 andDenominator:2] ;
+    goal = [second initWithNumerator:-1 andDenominator:6] ;
+    
+    answer = [second subtractFrom:first] ;
+    
+    if(answer == goal)
+        {
+            printf("1/3 - 1/2 = -1/6 succeeded \n");
+        }
+    else
+        {
+            printf("error \n");
+        }
+    
+    
+    
+    
     return NSApplicationMain(argc, (const char **)argv);
 }
