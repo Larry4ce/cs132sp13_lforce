96e7023fcf9dfe3d16ea3eabe261898c4a593495
Larry Force <lforce@Mac110-Rakdos.local>
2013-04-24 12:33:51 -0400

    Initial Fraction Calculator Setup Added

---
+++ Fraction Calculator/Fraction Calculator/Fraction Calculator/WCSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WCSAppDelegate.h
+//  Fraction Calculator
+//
+//  Created by Larry Force on 4/24/13.
+//  Copyright (c) 2013 Larry Force. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WCSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Fraction Calculator/Fraction Calculator/Fraction Calculator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Fraction Calculator
+//
+//  Created by Larry Force on 4/24/13.
+//  Copyright (c) 2013 Larry Force. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
